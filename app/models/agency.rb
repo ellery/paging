6bb594c06a8f52e58cb2111fd7a23caf92e53479
Prelang Builder <builder@prelang.com>
2015-08-26 04:09:50 +0000

    Defined validation for attribute 'phone' on model 'Agency'

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -2,4 +2,5 @@ class Agency < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_and_belongs_to_many :receivers
   validates_formatting_of :zip, using: :us_zip
+  validates_formatting_of :phone, using: :us_phone
 end
