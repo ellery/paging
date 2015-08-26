df96024fc4c1dba96b989c16a5c52d0b57e2e803
Prelang Builder <builder@prelang.com>
2015-08-26 04:10:10 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Receiver' to allow for create, update

--- app/admin/receiver.rb
+++ app/admin/receiver.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Receiver do
+  permit_params :location, :frequency, :latitude, :longitude, :haat
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
