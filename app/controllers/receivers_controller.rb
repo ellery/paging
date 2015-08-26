09a0f134342f311a03b6640acd72ca852de6210c
Prelang Builder <builder@prelang.com>
2015-08-26 04:10:06 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/receivers_controller.rb
+++ app/controllers/receivers_controller.rb
@@ -1,4 +1,8 @@
 class ReceiversController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_receiver, only: [:show, :edit, :update, :destroy]
 
   # GET /receivers
